fcd3e05c29fd569ea41f01f7a5d2bc4bd88a61e8
JoseStructuralEngineer <40444126+JoseStructuralEngineer@users.noreply.github.com>
2022-10-09 17:06:38 -0700

    Change Loop
    Change while loop for for loop

--- src/Game.java
+++ src/Game.java
@@ -2,55 +2,42 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
-
     public static void main(String[] args) {
-
         //Generate Random Number betwen 1 and 100
         Random random = new Random();
-        int valueToGuess = random.nextInt(101);
-        boolean finishGame = false;
-        int numOfTries = 0;
+        int max = 100;
+        int min = 1;
+        int valueToGuess = random.nextInt(max-min) + min;
         int diff = 100;
         System.out.println("Pick a number between 1 and 100");
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        while (!finishGame) {
-            //Convert to Integer
-            numOfTries = numOfTries + 1;
-            if (numOfTries <= 5) {
-                int reply = myObj.nextInt();
-                myObj.nextLine();
-                //Check if number is between 1 and 100
-                if (reply > 0 && reply < 101) {
-                    diff = reply - valueToGuess;
-                    //Note Only print Please pick a lower number or pick higher number if tries are less than 5
-                    if (diff > 0) {
-                        if (numOfTries < 5) {
-                            System.out.println("Please pick a lower number");
-                        }
-                    } else if (diff < 0) {
-                        if (numOfTries < 5) {
-                            System.out.println("Please pick a lower number");
-                        }
-                    } else {
-                        finishGame = true;
+        for(int i = 0;i<5;i++){
+            int reply = myObj.nextInt();
+            myObj.nextLine();
+            if (reply > 0 && reply < 101) {
+                diff = reply - valueToGuess;
+                //Note Only print Please pick a lower number or pick higher number if tries are less than 5
+                if (diff > 0) {
+                    if(i != 4){
+                        System.out.println("Please pick a lower number");
+                    }
+                } else if (diff < 0) {
+                    if(i != 4){
+                        System.out.println("Please pick a lower number");
                     }
                 } else {
-                    numOfTries = numOfTries - 1;
-                    System.out.println("Your guess is not between 1 and 100, please try again");
+                    i = 5;
                 }
             } else {
-                finishGame = true;
+                i = i - 1;
+                System.out.println("Your guess is not between 1 and 100, please try again");
             }
         }
-
         if (diff == 0) {
             System.out.println("You win!");
         } else {
             System.out.println("You lose!");
             System.out.println("The number to guess was: " + valueToGuess);
         }
-
-
-
     }
 }
